d160a5f33f6fb4c3818728e4688ba30aaa7a1795
ouaaitalla <houssamaitalla5@gmail.com>
2026-01-04 17:17:41 +0100

    v1.0

--- main.go
+++ main.go
@@ -7,10 +7,6 @@ import (
 	handel "ascii-art-web/Handler"
 )
 
-type Data struct {
-	Result string
-}
-
 func main() {
 	http.HandleFunc("/", handel.Home)
 	http.HandleFunc("/ascii-art", handel.Ascii_art)
